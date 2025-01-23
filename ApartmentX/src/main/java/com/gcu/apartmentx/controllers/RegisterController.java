99d5c24853e18ce3e35bd9bef3278a556a186368
Justin Iaconis <JIaconis@my.gcu.edu>
2025-01-22 17:39:40 -0700

    Project cleanup

--- ApartmentX/src/main/java/com/gcu/apartmentx/controllers/RegisterController.java
+++ ApartmentX/src/main/java/com/gcu/apartmentx/controllers/RegisterController.java
@@ -1,11 +1,13 @@
 package com.gcu.apartmentx.controllers;
 
 import com.gcu.apartmentx.models.ApartmentXUser;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@Controller
 public class RegisterController {
     @GetMapping("/register")
     public String register(Model model) {
