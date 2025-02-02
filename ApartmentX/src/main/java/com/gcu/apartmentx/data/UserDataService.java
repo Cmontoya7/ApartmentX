c436b7c5f53a170574403d165886abecff54aa23
Cmontoya7 <chris.l.montoya7@gmail.com>
2025-02-01 22:23:09 -0600

    Created the format for Managing User through the Admin User Profiles. Functional with no known bugs

--- ApartmentX/src/main/java/com/gcu/apartmentx/data/UserDataService.java
+++ ApartmentX/src/main/java/com/gcu/apartmentx/data/UserDataService.java
@@ -2,7 +2,6 @@ package com.gcu.apartmentx.data;
 
 import com.gcu.apartmentx.data.entities.UserEntity;
 import com.gcu.apartmentx.data.repositories.UserRepository;
-import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
