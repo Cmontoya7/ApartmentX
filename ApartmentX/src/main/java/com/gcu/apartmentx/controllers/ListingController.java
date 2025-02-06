35336c7cf700a7e31bef510bb24a713e2ca951ef
Jack Parkison <157999365+Sonnumber5@users.noreply.github.com>
2025-02-05 22:07:00 -0500

    Add files via upload

--- ApartmentX/src/main/java/com/gcu/apartmentx/controllers/ListingController.java
+++ ApartmentX/src/main/java/com/gcu/apartmentx/controllers/ListingController.java
@@ -7,7 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.gcu.apartmentx.business.ApartmentBean;
 import com.gcu.apartmentx.data.ApartmentDataService;
@@ -17,22 +20,40 @@ import com.gcu.apartmentx.models.ApartmentModel;
 @Controller
 public class ListingController {
 
+	private ApartmentEntity apartment;
+	
 	@Autowired
 	private ApartmentDataService apartmentService;
 	
+	
     @GetMapping("/listings")
     public String listings(Model model) {
-    	List<ApartmentEntity> apartmentEntities = apartmentService.findAll();
+    	List<ApartmentEntity> apartments = apartmentService.findAll();
     	
-    	List<ApartmentModel> listings = apartmentEntities.stream().map(entity -> new ApartmentModel(entity.getName(), entity.getNumBeds(), entity.getNumBaths(), entity.getFloorSpace(), entity.getPrice(), entity.getQuantity())).collect(Collectors.toList());
-    	model.addAttribute("title", "Listings");
-    	model.addAttribute("listings", listings);
+    	model.addAttribute("title", "Apartment Listings");
+    	model.addAttribute("apartments", apartments);
     	return "Listings";
     }
 
-    @GetMapping("/deletelisting")
-    public String deleteListing() {
-        return "DeleteListing";
+  //Post request (receiving from Listing.html) that handles when a user selects "Delete" on an apartment
+    @PostMapping("/deleteApartment/confirm")
+    public String confirmDeleteApartment(Model m, @RequestParam(value = "action", required = false) int id) {
+        apartment = apartmentService.findById(id); //passes the ApartmentEntity's "id" attribute to the findById method to find the selected item in the database
+        m.addAttribute("apartment", apartment); //passes the item (found by its id) to the ConfirmDeleteApartment HTML 
+        return "ConfirmDeleteApartment";
+    }
+
+    //Post request that handles the function of confirming the deletion of a selected apartment 
+    @PostMapping("/deleteApartment/isconfirmed")
+    public String doDeleteApartment(Model m, @RequestParam(value = "action", required = false) boolean confirm, RedirectAttributes redirectAttributes) {
+        if (confirm) { 
+        	apartmentService.delete(apartment.getId());
+            String msg = "Apartment: " + apartment.getName() + " was Deleted";
+            m.addAttribute("msg", msg);
+        }
+        List<ApartmentEntity> apartments = apartmentService.findAll();
+        m.addAttribute("apartments", apartments);
+        return "Listings";
     }
 
 }
